e219418da7a5a0ba27a8621c1c8004d6fc2b0849
Alan-Collins <cde3$rfvPj98h>
2020-05-08 13:46:52 -0500

    Added descriptions to scripts

--- Perform_LapA-like_Alignments/run_muscle_on_dir.py
+++ Perform_LapA-like_Alignments/run_muscle_on_dir.py
@@ -5,6 +5,19 @@ import subprocess
 
 
 def main(inDir, outDir):
+
+	"""
+	N.B. Requires a local installation of the MUSCLE alignment software available here (http://drive5.com/muscle/downloads.htm)
+
+	Perform MUSLCE alignments of the paired FASTA files produced by the create_paired_fastas.py script. Outputs clustalW format alignments to
+	specified outdir. Each file is named for example: "Pseudomonas_alcaligenes1_LapA.clw" where "1" indicates that this is the largest protein 
+	identified for that species and "LapA" indicates that the sequence of the protein was aligned with that of LapA.
+	inDir: Directory containing the paired FASTA files output by the create_paired_fastas.py script.
+	outDir: desired destination folder for alignments.
+
+	Example usage: python3 run_muscle_on_dir.py Paired_FASTAs Alignments
+	"""
+
 	for file in os.listdir(inDir):
 		outfilename = file[:-4] + ".clw"
 
